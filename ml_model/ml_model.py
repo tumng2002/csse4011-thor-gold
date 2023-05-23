c912516afc11ae734ef2a15f53414b620bbaba1a
GuchiEg <jamesdarcyhudson@gmail.com>
2023-05-24 02:42:49 +1000

    radar model

--- ml_model/ml_model.py
+++ ml_model/ml_model.py
@@ -2,47 +2,48 @@ import matplotlib.pyplot as plt
 import numpy as np 
 import tensorflow as tf
 from sklearn import metrics
+import pandas as pd
 
-from keras.optimizers import Adam
-from keras.models import Model
-from keras.layers import Dense
-from keras.layers import Input
-from keras.layers import Flatten
-from keras.layers import Conv2D
-from keras.layers.normalization import BatchNormalization
-from keras.layers import Dropout
-
-
-def define_CNN(in_shape, n_keypoints):
+# from keras.optimizers import Adam
+# from keras.models import Model
+# from keras.layers import Dense
+# from keras.layers import Input
+# from keras.layers import Flatten
+# from keras.layers import Conv2D
+# from keras.layers.normalization import BatchNormalization
+# from keras.layers import Dropout
 
 
-    in_one = Input(shape=in_shape)
-    conv_one_1 = Conv2D(16, kernel_size=(3, 3), activation='relu', strides=(1, 1), padding = 'same')(in_one)
-    conv_one_1 = Dropout(0.3)(conv_one_1)
-    conv_one_2 = Conv2D(32, kernel_size=(3, 3), activation='relu', strides=(1, 1), padding = 'same')(conv_one_1)
-    conv_one_2 = Dropout(0.3)(conv_one_2)
+# def define_CNN(in_shape, n_keypoints):
 
-    conv_one_2 = BatchNormalization(momentum=0.95)(conv_one_2)
 
-    fe = Flatten()(conv_one_2)
-    # dense1
-    dense_layer1 = Dense(512, activation='relu')(fe)
-    dense_layer1 = BatchNormalization(momentum=0.95)(dense_layer1)
-    # # dropout
+#     in_one = Input(shape=in_shape)
+#     conv_one_1 = Conv2D(16, kernel_size=(3, 3), activation='relu', strides=(1, 1), padding = 'same')(in_one)
+#     conv_one_1 = Dropout(0.3)(conv_one_1)
+#     conv_one_2 = Conv2D(32, kernel_size=(3, 3), activation='relu', strides=(1, 1), padding = 'same')(conv_one_1)
+#     conv_one_2 = Dropout(0.3)(conv_one_2)
 
-    # dropout
-    dense_layer1 = Dropout(0.4)(dense_layer1)
+#     conv_one_2 = BatchNormalization(momentum=0.95)(conv_one_2)
+
+#     fe = Flatten()(conv_one_2)
+#     # dense1
+#     dense_layer1 = Dense(512, activation='relu')(fe)
+#     dense_layer1 = BatchNormalization(momentum=0.95)(dense_layer1)
+#     # # dropout
+
+#     # dropout
+#     dense_layer1 = Dropout(0.4)(dense_layer1)
     
-    out_layer = Dense(n_keypoints, activation = 'linear')(dense_layer1)
+#     out_layer = Dense(n_keypoints, activation = 'linear')(dense_layer1)
     
 
-    # model
-    model = Model(in_one, out_layer)
-    opt = Adam(lr=0.001, beta_1=0.5)
+#     # model
+#     model = Model(in_one, out_layer)
+#     opt = Adam(lr=0.001, beta_1=0.5)
 
-    # compile the model
-    model.compile(loss='mse', optimizer=opt, metrics=['mae', 'mse', 'mape', tf.keras.metrics.RootMeanSquaredError()])
-    return model
+#     # compile the model
+#     model.compile(loss='mse', optimizer=opt, metrics=['mae', 'mse', 'mape', tf.keras.metrics.RootMeanSquaredError()])
+#     return model
 
 
 def main():
@@ -65,18 +66,49 @@ def main():
 
     # load model
     keypoint_model = tf.keras.models.load_model("model/MARS.h5")
-    keypoint_model.predict(labels_test[0])
+    # Repeat i iteration to get the average result
+    # for i in range(10):
+    # instantiate the model
+    keypoint_model = tf.keras.models.load_model("model/MARS.h5")
+
+    # save and print the metrics
+    # score_train = keypoint_model.evaluate(featuremap_train, labels_train,verbose = 1)
+    # print('train MAPE = ', score_train[3])
+    # score_test = keypoint_model.evaluate(featuremap_test, labels_test,verbose = 1)
+    # print('test MAPE = ', score_test[3])
+    print(len(featuremap_test))
+    result_test = keypoint_model.predict(np.array([featuremap_test[2735]]))
+
+    x = result_test[0][0:19]
+    y = result_test[0][19:38]
+    z = result_test[0][38:57]
+    # print(pts)
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    ax.scatter(x, y, z)
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
+    plt.show()
+
+    # df = pd.DataFrame(result_test)
+    # print(df)
+    # print(featuremap_test)
+    # print(featuremap_test[0])
+    # print(result_test)
+    # print(result_test[0])
     exit()
 
     # instantiate the model
-    keypoint_model = define_CNN(featuremap_train[0].shape, 57)
+    # keypoint_model = define_CNN(featuremap_train[0].shape, 57)
 
-    # # initial maximum error 
-    # score_min = 10
-    history = keypoint_model.fit(featuremap_train, labels_train,
-                                batch_size=batch_size, epochs=epochs, verbose=1, 
-                                validation_data=(featuremap_validate, labels_validate))
-    result_test = keypoint_model.predict(featuremap_test)
+    # # # initial maximum error 
+    # # score_min = 10
+    # history = keypoint_model.fit(featuremap_train, labels_train,
+    #                             batch_size=batch_size, epochs=epochs, verbose=1, 
+    #                             validation_data=(featuremap_validate, labels_validate))
+    # result_test = keypoint_model.predict(featuremap_test)
 
 if __name__ == "__main__":
     main()
--- mmw_radar/readData_AWR1843.py
+++ mmw_radar/readData_AWR1843.py
@@ -1,12 +1,15 @@
 import serial
 import time
 import numpy as np
-import pyqtgraph as pg
-from pyqtgraph.Qt import QtGui
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from sklearn import metrics
+import pandas as pd
 
 # Change the configuration file name
 # configFileName = 'AWR1843config.cfg'
-configFileName = "tuned-radar.cfg"
+# configFileName = "tuned-radar.cfg"
+configFileName = "profile_3d.cfg"
 
 CLIport = {}
 Dataport = {}
@@ -189,6 +192,11 @@ def readAndParseData18xx(Dataport, configParameters):
         subFrameNumber = np.matmul(byteBuffer[idX:idX+4],word)
         idX += 4
 
+        x = np.zeros(numDetectedObj,dtype=np.float32)
+        y = np.zeros(numDetectedObj,dtype=np.float32)
+        z = np.zeros(numDetectedObj,dtype=np.float32)
+        velocity = np.zeros(numDetectedObj,dtype=np.float32)
+
         # Read the TLV messages
         for tlvIdx in range(numTLVs):
             
@@ -200,16 +208,18 @@ def readAndParseData18xx(Dataport, configParameters):
             idX += 4
             tlv_length = np.matmul(byteBuffer[idX:idX+4],word)
             idX += 4
+            print(tlv_type)
+            
 
             # Read the data depending on the TLV message
             if tlv_type == MMWDEMO_UART_MSG_DETECTED_POINTS:
 
                 # Initialize the arrays
-                x = np.zeros(numDetectedObj,dtype=np.float32)
-                y = np.zeros(numDetectedObj,dtype=np.float32)
-                z = np.zeros(numDetectedObj,dtype=np.float32)
-                velocity = np.zeros(numDetectedObj,dtype=np.float32)
-                intensity = np.zeros(numDetectedObj,dtype=np.float32)
+                # x = np.zeros(numDetectedObj,dtype=np.float32)
+                # y = np.zeros(numDetectedObj,dtype=np.float32)
+                # z = np.zeros(numDetectedObj,dtype=np.float32)
+                # velocity = np.zeros(numDetectedObj,dtype=np.float32)
+                # intensity = np.zeros(numDetectedObj,dtype=np.float32)
                 
                 for objectNum in range(numDetectedObj):
                     
@@ -222,11 +232,31 @@ def readAndParseData18xx(Dataport, configParameters):
                     idX += 4
                     velocity[objectNum] = byteBuffer[idX:idX + 4].view(dtype=np.float32)
                     idX += 4
-                    intensity[objectNum] = byteBuffer[idX:idX + 4].view(dtype=np.float32)
-                    idX += 4
                 
                 # Store the data in the detObj dictionary
-                detObj = {"numObj": numDetectedObj, "x": x, "y": y, "z": z, "velocity":velocity, "intensity":intensity}
+                # detObj = {"numObj": numDetectedObj, "x": x, "y": y, "z": z, "velocity":velocity}
+                # dataOK = 1
+            
+            # NOISE ALWAYS COMES AFTER POINT CLOUT
+            elif tlv_type == 7:
+                """
+                Data Fields
+                int16_t 	snr
+                    snr - CFAR cell to side noise ratio in dB expressed in 0.1 steps of dB
+                
+                int16_t 	noise
+                    y - CFAR noise level of the side of the detected cell in dB expressed in 0.1 steps of dB
+                """
+                noise = np.zeros(numDetectedObj,dtype=np.uint16)
+                snr = np.zeros(numDetectedObj,dtype=np.uint16)
+                for objectNum in range(numDetectedObj):
+                    
+                    # Read the data for each object
+                    snr[objectNum] = byteBuffer[idX:idX + 2].view(dtype=np.uint16)
+                    idX += 2
+                    noise[objectNum] = byteBuffer[idX:idX + 2].view(dtype=np.uint16)
+                    idX += 2
+                detObj = {"x": x, "y": y, "z": z, "velocity":velocity, "snr": snr, "noise": noise}
                 dataOK = 1
                 
  
@@ -243,7 +273,7 @@ def readAndParseData18xx(Dataport, configParameters):
             if byteBufferLength < 0:
                 byteBufferLength = 0         
 
-    return dataOK, frameNumber, detObj
+    return dataOK, frameNumber, detObj, numDetectedObj
 
 # ------------------------------------------------------------------
 
@@ -251,23 +281,11 @@ def readAndParseData18xx(Dataport, configParameters):
 def update():
      
     dataOk = 0
-    global detObj
-    x = []
-    y = []
       
     # Read and parse the received data
-    dataOk, frameNumber, detObj = readAndParseData18xx(Dataport, configParameters)
+    dataOk, _, detObj, numDetectedObj = readAndParseData18xx(Dataport, configParameters)
     
-    if dataOk and len(detObj["x"])>0:
-        print(detObj["numObj"])
-        # print(frameNumber)
-        x = -detObj["x"]
-        y = detObj["y"]
-        
-        s.setData(x,y)
-        QtGui.QApplication.processEvents()
-    
-    return dataOk
+    return dataOk, detObj, numDetectedObj
 
 
 # -------------------------    MAIN   -----------------------------------------  
@@ -278,43 +296,83 @@ CLIport, Dataport = serialConfig(configFileName)
 # Get the configuration parameters from the configuration file
 configParameters = parseConfigFile(configFileName)
 
-# START QtAPPfor the plot
-app = QtGui.QApplication([])
-
-# Set the plot 
-pg.setConfigOption('background','w')
-win = pg.GraphicsLayoutWidget(title="2D scatter plot")
-p = win.addPlot()
-p.setXRange(-0.5,0.5)
-p.setYRange(0,1.5)
-p.setLabel('left',text = 'Y position (m)')
-p.setLabel('bottom', text= 'X position (m)')
-s = p.plot([],[],pen=None,symbol='o')
-win.show()
+keypoint_model = tf.keras.models.load_model("../ml_model/model/MARS.h5")
    
-# Main loop 
-detObj = {}  
+# Main loop
 frameData = {}    
 currentIndex = 0
+
+fig = plt.figure()
+
+ax = fig.add_subplot(projection='3d')
+plot = True
+
 while True:
     try:
         # Update the data and check if the data is okay
-        dataOk = update()
+        dataOk, detObj, numDetectedObj = update()
         # print(dataOk)
-        
+
         if dataOk:
             # Store the current frame into frameData
             frameData[currentIndex] = detObj
             currentIndex += 1
+            x_raw = detObj["x"]
+            y_raw = detObj["y"]
+            z_raw = detObj["z"]
+            point_cloud = pd.DataFrame(detObj)
+            point_cloud["intensity"] = point_cloud["snr"] / 10
+            # sort by x, then y for equal x, then z for equal x and y
+            point_cloud = point_cloud.sort_values(by=["x", "y", "z"])
+            # drop values based on -1 < x < 1
+            point_cloud = point_cloud.drop(point_cloud[point_cloud.x < -1].index)
+            point_cloud = point_cloud.drop(point_cloud[point_cloud.x > 1].index)
+            # drop values based on -1 < z < 1
+            point_cloud = point_cloud.drop(point_cloud[point_cloud.z < -1].index)
+            point_cloud = point_cloud.drop(point_cloud[point_cloud.z > 1].index)
+            # drop values based on 0 < y < 2
+            point_cloud = point_cloud.drop(point_cloud[point_cloud.y < 0].index)
+            point_cloud = point_cloud.drop(point_cloud[point_cloud.y > 2].index)
+            point_cloud = point_cloud.reset_index()
+            point_cloud = point_cloud.drop(["index", "snr", "noise"], axis=1)
+            # cut data to first 64 points, and zero pad if under
+            df_final = point_cloud.reindex(range(64), fill_value=0)
+            # convert dataframe to 64x5 column vector
+            column_vector = df_final.values
+            # convert column vector to square 8x8x5 matrix in row-major order
+            square_matrix = np.array([column_vector.reshape(8, 8, 5)])
+            skeleton = keypoint_model.predict(square_matrix)
+            x = skeleton[0][0:19]
+            y = skeleton[0][19:38]
+            z = skeleton[0][38:57]
+
+            if plot:
+                ax.clear()
+                ax.scatter(x, y, z)
+                ax.scatter(x_raw, y_raw, z_raw)
+                ax.set_xlabel('X Label')
+                ax.set_ylabel('Y Label')
+                ax.set_zlabel('Z Label')
+                ax.axes.set_xlim3d(left=-1, right=1) 
+                ax.axes.set_ylim3d(bottom=0, top=3) 
+                ax.axes.set_zlim3d(bottom=-1, top=1) 
+                plt.pause(0.05)
+        # Update the data and check if the data is okay
+        # dataOk = update()
+        # # print(dataOk)
         
-        time.sleep(0.05) # Sampling frequency of 30 Hz
+        # if dataOk:
+        #     # Store the current frame into frameData
+        #     frameData[currentIndex] = detObj
+        #     currentIndex += 1
+        
+        # time.sleep(0.05) # Sampling frequency of 30 Hz
         
     # Stop the program and close everything if Ctrl + c is pressed
     except KeyboardInterrupt:
         CLIport.write(('sensorStop\n').encode())
         CLIport.close()
         Dataport.close()
-        win.close()
         break
         
     

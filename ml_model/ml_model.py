81b5de21418b3e2d6080938a985ee7b3614d1e53
Tu Nguyen <tu.nguyen2@uqconnect.edu.au>
2023-05-25 12:46:05 +1000

    ML training data collection

--- ml_model/ml_model.py
+++ ml_model/ml_model.py
@@ -4,57 +4,58 @@ import tensorflow as tf
 from sklearn import metrics
 import pandas as pd
 
-# from keras.optimizers import Adam
-# from keras.models import Model
-# from keras.layers import Dense
-# from keras.layers import Input
-# from keras.layers import Flatten
-# from keras.layers import Conv2D
-# from keras.layers.normalization import BatchNormalization
-# from keras.layers import Dropout
+from keras.optimizers import Adam
+from keras.models import Model
+from keras.layers import Dense
+from keras.layers import Input
+from keras.layers import Flatten
+from keras.layers import Conv2D
+from keras.layers.normalization import BatchNormalization
+from keras.layers import Dropout
 
 
-# def define_CNN(in_shape, n_keypoints):
+def define_CNN(in_shape, n_keypoints):
 
+    in_one = Input(shape=in_shape)
+    conv_one_1 = Conv2D(16, kernel_size=(3, 3), activation='relu', strides=(1, 1), padding = 'same')(in_one)
+    conv_one_1 = Dropout(0.3)(conv_one_1)
+    conv_one_2 = Conv2D(32, kernel_size=(3, 3), activation='relu', strides=(1, 1), padding = 'same')(conv_one_1)
+    conv_one_2 = Dropout(0.3)(conv_one_2)
 
-#     in_one = Input(shape=in_shape)
-#     conv_one_1 = Conv2D(16, kernel_size=(3, 3), activation='relu', strides=(1, 1), padding = 'same')(in_one)
-#     conv_one_1 = Dropout(0.3)(conv_one_1)
-#     conv_one_2 = Conv2D(32, kernel_size=(3, 3), activation='relu', strides=(1, 1), padding = 'same')(conv_one_1)
-#     conv_one_2 = Dropout(0.3)(conv_one_2)
+    conv_one_2 = BatchNormalization(momentum=0.95)(conv_one_2)
 
-#     conv_one_2 = BatchNormalization(momentum=0.95)(conv_one_2)
+    fe = Flatten()(conv_one_2)
+    # dense1
+    dense_layer1 = Dense(512, activation='relu')(fe)
+    dense_layer1 = BatchNormalization(momentum=0.95)(dense_layer1)
+    # # dropout
 
-#     fe = Flatten()(conv_one_2)
-#     # dense1
-#     dense_layer1 = Dense(512, activation='relu')(fe)
-#     dense_layer1 = BatchNormalization(momentum=0.95)(dense_layer1)
-#     # # dropout
-
-#     # dropout
-#     dense_layer1 = Dropout(0.4)(dense_layer1)
+    # dropout
+    dense_layer1 = Dropout(0.4)(dense_layer1)
     
-#     out_layer = Dense(n_keypoints, activation = 'linear')(dense_layer1)
+    out_layer = Dense(n_keypoints, activation = 'linear')(dense_layer1)
     
 
-#     # model
-#     model = Model(in_one, out_layer)
-#     opt = Adam(lr=0.001, beta_1=0.5)
+    # model
+    model = Model(in_one, out_layer)
+    opt = Adam(lr=0.001, beta_1=0.5)
 
-#     # compile the model
-#     model.compile(loss='mse', optimizer=opt, metrics=['mae', 'mse', 'mape', tf.keras.metrics.RootMeanSquaredError()])
-#     return model
+    # compile the model
+    model.compile(loss='mse', optimizer=opt, metrics=['mae', 'mse', 'mape', tf.keras.metrics.RootMeanSquaredError()])
+    return model
 
 
 def main():
-    #load the feature and labels, 24066, 8033, and 7984 frames for train, validate, and test
-    featuremap_train = np.load('feature/featuremap_train.npy')
-    featuremap_validate = np.load('feature/featuremap_validate.npy')
-    featuremap_test = np.load('feature/featuremap_test.npy')
+    # #load the feature and labels, 24066, 8033, and 7984 frames for train, validate, and test
+    # featuremap_train = np.load('feature/featuremap_train.npy')
+    # featuremap_validate = np.load('feature/featuremap_validate.npy')
+    # featuremap_test = np.load('feature/featuremap_test.npy')
 
-    labels_train = np.load('feature/labels_train.npy')
-    labels_validate = np.load('feature/labels_validate.npy')
-    labels_test = np.load('feature/labels_test.npy')
+    # labels_train = np.load('feature/labels_train.npy')
+    # labels_validate = np.load('feature/labels_validate.npy')
+    # labels_test = np.load('feature/labels_test.npy')
+
+
 
     # Initialize the result array
     paper_result_list = []
